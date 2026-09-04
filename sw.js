b2fc5b61df3a7235f99946b8640dc7daa738ce0a
yseungae <yseungae@users.noreply.github.com>
2026-09-04 10:12:23 -0400

    style: compact record photo thumbnails

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'dayfill-v8';
+const CACHE_VERSION = 'dayfill-v9';
 const ASSETS = [
   './', './index.html', './styles.css', './app.js', './manifest.webmanifest',
   './icons/icon.svg', './icons/icon-192.png', './icons/icon-512.png'
